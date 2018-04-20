6297dc46ccf211546c36e4b2fc7675e2093fcba8
yuanhe772 <31265339+yuanhe772@users.noreply.github.com>
2018-04-19 21:32:21 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * main.java, ECE4960-P4
+ * Created by Yuan He(yh772) on 2018/04/19
+ * Platform: Java 8, Eclipse, MacOS
+ * Copyright © 2018 Yuan He. All rights reserved.
+ */
+public class Main {
+
+	
+	public static void main(String args[]) {
+		/* Task 1 - Task 3: */
+		
+		// Testing ODE solvers, including RK34, RK4, Forward-Euler
+		Test.validateODE();
+		
+	}
+}
